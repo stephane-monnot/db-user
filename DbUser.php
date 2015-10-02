50ee8e3c3ea2ca37e10942b67fc5c0367efb320b
Monnot Stéphane <monnot.stephane@gmail.com>
2015-10-02 18:25:08 +0200

    Initial import

---
+++ DbUser.php
@@ -0,0 +1,363 @@
+<?php
+namespace Shinbuntu\DbUser;
+
+use Doctrine\DBAL\Connection as DoctrineConnection;
+
+/**
+ * Create sql users
+ *
+ * @author  Stéphane Monnot <smonnot@solire.fr>
+ * @license MIT http://mit-license.org/
+ */
+class DbUser
+{
+    /**
+     * Constant for privilege CREATE.
+     */
+    const PRIVILEGE_CREATE = 'CREATE';
+
+    /**
+     * Constant for privilege DROP.
+     */
+    const PRIVILEGE_DROP = 'DROP';
+
+    /**
+     * Constant for privilege GRANT OPTION.
+     */
+    const PRIVILEGE_GRANT_OPTION = 'GRANT OPTION';
+
+    /**
+     * Constant for privilege LOCK TABLES.
+     */
+    const PRIVILEGE_LOCK_TABLES = 'LOCK TABLES';
+
+    /**
+     * Constant for privilege REFERENCES.
+     */
+    const PRIVILEGE_REFERENCES = 'REFERENCES';
+
+    /**
+     * Constant for privilege EVENT.
+     */
+    const PRIVILEGE_EVENT = 'EVENT';
+
+    /**
+     * Constant for privilege ALTER.
+     */
+    const PRIVILEGE_ALTER = 'ALTER';
+
+    /**
+     * Constant for privilege DELETE.
+     */
+    const PRIVILEGE_DELETE = 'DELETE';
+
+    /**
+     * Constant for privilege INDEX.
+     */
+    const PRIVILEGE_INDEX = 'INDEX';
+
+    /**
+     * Constant for privilege INSERT.
+     */
+    const PRIVILEGE_INSERT = 'INSERT';
+
+    /**
+     * Constant for privilege SELECT.
+     */
+    const PRIVILEGE_SELECT = 'SELECT';
+
+    /**
+     * Constant for privilege UPDATE.
+     */
+    const PRIVILEGE_UPDATE = 'UPDATE';
+
+    /**
+     * Constant for privilege CREATE TEMPORARY TABLES.
+     */
+    const PRIVILEGE_CREATE_TEMPORARY_TABLES = 'CREATE TEMPORARY TABLES';
+
+    /**
+     * Constant for privilege TRIGGER.
+     */
+    const PRIVILEGE_TRIGGER = 'TRIGGER';
+
+    /**
+     * Constant for privilege CREATE VIEW.
+     */
+    const PRIVILEGE_CREATE_VIEW = 'CREATE VIEW';
+
+    /**
+     * Constant for privilege SHOW VIEW.
+     */
+    const PRIVILEGE_SHOW_VIEW = 'SHOW VIEW';
+
+    /**
+     * Constant for privilege ALTER ROUTINE.
+     */
+    const PRIVILEGE_ALTER_ROUTINE = 'ALTER ROUTINE';
+
+    /**
+     * Constant for privilege CREATE ROUTINE.
+     */
+    const PRIVILEGE_CREATE_ROUTINE = 'CREATE ROUTINE';
+
+    /**
+     * Constant for privilege EXECUTE.
+     */
+    const PRIVILEGE_EXECUTE = 'EXECUTE';
+
+    /**
+     * Constant for privilege FILE.
+     */
+    const PRIVILEGE_FILE = 'FILE';
+
+    /**
+     * Constant for privilege CREATE USER.
+     */
+    const PRIVILEGE_CREATE_USER = 'CREATE USER';
+
+    /**
+     * Constant for privilege PROCESS.
+     */
+    const PRIVILEGE_PROCESS = 'PROCESS';
+
+    /**
+     * Constant for privilege RELOAD.
+     */
+    const PRIVILEGE_RELOAD = 'RELOAD';
+
+    /**
+     * Constant for privilege REPLICATION CLIENT.
+     */
+    const PRIVILEGE_REPLICATION_CLIENT = 'REPLICATION CLIENT';
+
+    /**
+     * Constant for privilege REPLICATION SLAVE.
+     */
+    const PRIVILEGE_REPLICATION_SLAVE = 'REPLICATION SLAVE';
+
+    /**
+     * Constant for privilege SHOW DATABASES.
+     */
+    const PRIVILEGE_SHOW_DATABASES = 'SHOW DATABASES';
+
+    /**
+     * Constant for privilege SHUTDOWN.
+     */
+    const PRIVILEGE_SHUTDOWN = 'SHUTDOWN';
+
+    /**
+     * Constant for privilege SUPER.
+     */
+    const PRIVILEGE_SUPER = 'SUPER';
+
+    /**
+     * Constant for privilege ALL.
+     */
+    const PRIVILEGE_ALL = 'ALL';
+
+    /**
+     * Constant for privilege USAGE.
+     */
+    const PRIVILEGE_USAGE = 'USAGE';
+
+    /**
+     * Constant for privilege statement GRANT.
+     */
+    const PRIVILEGE_STATEMENT_GRANT = 'GRANT';
+
+    /**
+     * Constant for privilege statement REVOKE.
+     */
+    const PRIVILEGE_STATEMENT_REVOKE = 'REVOKE';
+
+    /**
+     * The connection
+     *
+     * @var DoctrineConnection
+     */
+    protected $connection;
+
+    /**
+     * Constructor
+     *
+     * @param DoctrineConnection $connection The connection
+     */
+    public function __construct(DoctrineConnection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * Create MYSQL user
+     *
+     * @param string $username Mysql username
+     * @param string $password Mysql password
+     *
+     * @return boolean TRUE on success or FALSE on failure.
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function createUser($username, $password)
+    {
+        return $this->connection->exec($this->createUserQuery($username, $password));
+    }
+
+    /**
+     * Build query to create MYSQL user
+     *
+     * @param string $username Mysql username
+     * @param string $password Mysql password
+     *
+     * @return string SQL Query string
+     */
+    public function createUserQuery($username, $password)
+    {
+        return 'CREATE USER ' . $username . '@localhost IDENTIFIED BY ' . $this->connection->quote($password) . ';';
+    }
+
+    /**
+     * Delete MYSQL user
+     *
+     * @param string $username Mysql username
+     *
+     * @return boolean TRUE if exist or FALSE if not.
+     */
+    public function dropUser($username)
+    {
+        return $this->connection->exec($this->dropUserQuery($username));
+    }
+
+    /**
+     * Build query to drop MYSQL user
+     *
+     * @param string $username Mysql username
+     *
+     * @return string SQL Query string
+     */
+    public function dropUserQuery($username)
+    {
+        return 'DROP USER ' . $username . '@localhost;';
+    }
+
+    /**
+     * Test if MYSQL user exist
+     *
+     * @param string $username Mysql username
+     *
+     * @return boolean TRUE if exist or FALSE if not.
+     */
+    public function userExist($username)
+    {
+        return $this->connection->fetchColumn($this->userExistQuery($username));
+    }
+
+    /**
+     * Build query to test if MYSQL user exist
+     *
+     * @param string $username Mysql username
+     *
+     * @return string SQL Query string
+     */
+    public function userExistQuery($username)
+    {
+        return 'SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = ' . $this->connection->quote($username) . ');';
+    }
+
+    /**
+     * Grant privileges to mysql user
+     *
+     * @param string       $username   Mysql username
+     * @param array|string $privileges Mysql privileges
+     * @param string       $database   Mysql database name
+     * @param string       $table      Mysql $table name
+     *
+     * @return boolean TRUE on success or FALSE on failure.
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function grantPrivileges($username, $privileges = self::PRIVILEGE_USAGE, $database = '*', $table = '*')
+    {
+        $sqlQuery = $this->changePrivilegesQuery(
+            self::PRIVILEGE_STATEMENT_GRANT,
+            $username,
+            $privileges,
+            $database,
+            $table
+        );
+
+        return $this->connection->exec($sqlQuery);
+    }
+
+    /**
+     * Revoke privileges to mysql user
+     *
+     * @param string       $username   Mysql username
+     * @param array|string $privileges Mysql privileges
+     * @param string       $database   Mysql database name
+     * @param string       $table      Mysql $table name
+     *
+     * @return boolean TRUE on success or FALSE on failure.
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function revokePrivileges($username, $privileges = self::PRIVILEGE_USAGE, $database = '*', $table = '*')
+    {
+        $sqlQuery = $this->changePrivilegesQuery(
+            self::PRIVILEGE_STATEMENT_REVOKE,
+            $username,
+            $privileges,
+            $database,
+            $table
+        );
+
+        return $this->connection->exec($sqlQuery);
+    }
+
+    /**
+     * Flush privileges
+     *
+     * @return boolean TRUE on success or FALSE on failure.
+     */
+    public function flushPrivileges()
+    {
+        return $this->connection->exec($this->flushPrivilegesQuery());
+    }
+
+    /**
+     * Build query to flush privileges
+     *
+     * @return string SQL Query string
+     */
+    public function flushPrivilegesQuery()
+    {
+        return 'FLUSH PRIVILEGES;';
+    }
+
+    /**
+     * Build query to Grant or Revoke privileges to mysql user
+     *
+     * @param string       $privilegeStatement REVOKE or GRANT
+     * @param string       $username           Mysql username
+     * @param array|string $privileges         Mysql privileges
+     * @param string       $database           Mysql database name
+     * @param string       $table              Mysql $table name
+     *
+     * @return string SQL Query string
+     */
+    public function changePrivilegesQuery(
+        $privilegeStatement,
+        $username,
+        $privileges = self::PRIVILEGE_USAGE,
+        $database = '*',
+        $table = '*'
+    ) {
+        if (is_string($privileges)) {
+            $privileges = [$privileges];
+        }
+
+        $usernameQuoted = $this->connection->quote($username);
+
+        $sqlQuery = $privilegeStatement . ' ' . implode(', ', $privileges)
+            . ' ON ' . $database . '.' . $table . ' TO ' . $usernameQuoted . '@localhost;';
+
+        return $sqlQuery;
+    }
+}
---
+++ Tests/Units/DbUser.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Test class for DbUser.
+ *
+ * @author  Stéphane Monnot <smonnot@solire.fr>
+ * @license MIT http://mit-license.org/
+ */
+
+namespace Shinbuntu\DbUser\Tests\Units;
+
+use atoum;
+use Shinbuntu\DbUser\DbUser as testedClass;
+use \mock\Doctrine\DBAL\Connection;
+
+/**
+ * Test class for DbUser.
+ *
+ * @author  Stéphane Monnot <smonnot@solire.fr>
+ * @license MIT http://mit-license.org/
+ */
+class DbUser extends atoum
+{
+    private $connection = null;
+
+    private function getConnection()
+    {
+        if ($this->connection !== null) {
+            return $this->connection;
+        }
+        $this->mockGenerator->shuntParentClassCalls();
+        $this->mockGenerator->orphanize('__construct');
+        $this->mockGenerator->orphanize('__construct');
+
+        $this->connection = new Connection;
+        $this->connection->getMockController()->connect = function() {};
+        $this->connection->getMockController()->fetchColumn = function() {};
+        $this->connection->getMockController()->quote = function($input) {
+            return '"' . addslashes($input) . '"';
+        };
+
+        $this->mockGenerator->unshuntParentClassCalls();
+
+        return $this->connection;
+    }
+
+    /**
+     * Test createUserQuery method
+     *
+     * @return void
+     */
+    public function testCreateUserQuery()
+    {
+        $connection = $this->getConnection();
+
+        $this
+            ->if($this->newTestedInstance($connection))
+            ->string($this->testedInstance->createUserQuery('test_username', '!super_secure_password$'))
+                ->isEqualTo('CREATE USER test_username@localhost IDENTIFIED BY "!super_secure_password$";')
+        ;
+    }
+
+    /**
+     * Test dropUser method
+     *
+     * @return void
+     */
+    public function testDropUserQuery()
+    {
+        $connection = $this->getConnection();
+
+        $this
+            ->if($this->newTestedInstance($connection))
+            ->string($this->testedInstance->dropUserQuery('test_username', '!super_secure_password$'))
+                ->isEqualTo('DROP USER test_username@localhost;')
+        ;
+    }
+
+    /**
+     * Test userExistQuery method
+     *
+     * @return void
+     */
+    public function testUserExistQuery()
+    {
+        $connection = $this->getConnection();
+
+        $this
+            ->if($this->newTestedInstance($connection))
+            ->string($this->testedInstance->userExistQuery('test_username'))
+                ->isEqualTo('SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = "test_username");')
+        ;
+    }
+
+    /**
+     * Test flushPrivilegesQuery method
+     *
+     * @return void
+     */
+    public function testFlushPrivilegesQuery()
+    {
+        $connection = $this->getConnection();
+
+        $this
+            ->if($this->newTestedInstance($connection))
+            ->string($this->testedInstance->flushPrivilegesQuery())
+                ->isEqualTo('FLUSH PRIVILEGES;')
+        ;
+    }
+
+    /**
+     * Test changePrivilegesQuery method
+     *
+     * @return void
+     */
+    public function testChangePrivilegesQuery()
+    {
+        $connection = $this->getConnection();
+
+        $this
+            ->if($this->newTestedInstabuild-toolsnce($connection))
+            ->string(
+                $this->testedInstance->changePrivilegesQuery(
+                    testedClass::PRIVILEGE_STATEMENT_GRANT,
+                    'test_username',
+                    [
+                        testedClass::PRIVILEGE_CREATE,
+                        testedClass::PRIVILEGE_UPDATE,
+                        testedClass::PRIVILEGE_DELETE,
+                    ],
+                    'test_database',
+                    'test_table'
+            ))
+                ->isEqualTo('GRANT CREATE, UPDATE, DELETE ON test_database.test_table TO "test_username"@localhost;')
+        ;
+    }
+}
